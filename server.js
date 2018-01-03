df6eee483f6f81d8af069c335bad4b48e18a7904
rayGreycloud <rayGreycloud@gmail.com>
2018-01-03 09:02:23 -0800

    Add basic server with dummy api

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/api/customers', (req, res) => {
+  // Dummy data for demo
+  const customers = [
+    {id: 001, firstName: 'Mickey', lastname: 'Mouse'},
+    
+    {id: 002, firstName: 'Donald', lastname: 'Duck'},
+    
+    {id: 003, firstName: 'Goofy', lastname: 'Goof'}
+  ];
+  
+  res.json(customers);
+});
+
+const port = 5000;
+
+app.listen(port, () => {
+  console.log(`Express listening on port ${port}`);
+});
+
